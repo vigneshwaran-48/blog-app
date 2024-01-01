40b0a90c867fd9dae3b846bcf692bcc4b4f505ef
vigneshwaran48 <p3487260@gmail.com>
2024-01-01 22:34:39 +0530

    Added google OAuth

--- application-service/src/main/java/com/vicky/blog/config/SecurityConfig.java
+++ application-service/src/main/java/com/vicky/blog/config/SecurityConfig.java
@@ -1,43 +1,43 @@
-// package com.vicky.blog.config;
+package com.vicky.blog.config;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.oauth2.jwt.JwtDecoder;
-// import org.springframework.security.oauth2.jwt.JwtDecoders;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
 
-// @EnableWebSecurity
-// @Configuration
-// public class SecurityConfig {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
 
-//     @Value("${oauth2.server.url}")
-//     private String oauth2ServerURL;
+    @Value("${oauth2.server.url}")
+    private String oauth2ServerURL;
 
-//     private static final Long MAX_AGE = 3600L;
+    private static final Long MAX_AGE = 3600L;
 
-//     @Bean
-//     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-//         return httpSecurity
-//                     .sessionManagement()
-//                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                     .and()
-//                     .csrf().disable()
-//                     .authorizeHttpRequests(http -> {
-//                         http
-//                             .anyRequest().authenticated();
-//                     })
-//                     .oauth2ResourceServer(oauth2 -> oauth2.jwt())
-//                     .build();
-//     }
+        return httpSecurity
+                    .sessionManagement()
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .csrf().disable()
+                    .authorizeHttpRequests(http -> {
+                        http
+                            .anyRequest().authenticated();
+                    })
+                    .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+                    .build();
+    }
 
-//     @Bean
-// 	JwtDecoder jwtDecoder() {
-// 	    return JwtDecoders.fromIssuerLocation(oauth2ServerURL);
-// 	}
+    @Bean
+	JwtDecoder jwtDecoder() {
+	    return JwtDecoders.fromIssuerLocation("https://accounts.google.com");
+	}
     
-// }
+}
