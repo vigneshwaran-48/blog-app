30484e26bf484295976da093123b64d43bb34c60
vigneshwaran48 <p3487260@gmail.com>
2024-03-12 21:53:55 +0530

    Firebase profile id changes

--- application-service/src/main/java/com/vicky/blog/repository/firebase/ProfileIdRepositorImpl.java
+++ application-service/src/main/java/com/vicky/blog/repository/firebase/ProfileIdRepositorImpl.java
@@ -2,8 +2,11 @@ package com.vicky.blog.repository.firebase;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Function;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
@@ -12,12 +15,20 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.stereotype.Repository;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Filter;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 import com.vicky.blog.model.ProfileId;
 import com.vicky.blog.repository.ProfileIdRepository;
 
 @Repository
 @Profile("prod")
 public class ProfileIdRepositorImpl implements ProfileIdRepository {
+
+    private static final String COLLECTION_NAME = "profile_id";
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProfileIdRepositorImpl.class);
 
     @Override
     public void flush() {
@@ -201,26 +212,77 @@ public class ProfileIdRepositorImpl implements ProfileIdRepository {
 
     @Override
     public Optional<ProfileId> findByEntityId(String entityId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findByEntityId'");
+        Firestore firestore = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> result =
+                firestore.collection(COLLECTION_NAME).whereEqualTo("entity_id", entityId).limit(1).get();
+        try {
+            List<ProfileId> profileIds = result.get().toObjects(ProfileId.class);
+            if (profileIds.isEmpty()) {
+                return Optional.empty();
+            }
+            return Optional.of(profileIds.get(0));
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (ExecutionException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return Optional.empty();
     }
 
     @Override
     public boolean existsByProfileId(String profileId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'existsByProfileId'");
+        Firestore firestore = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> result =
+                firestore.collection(COLLECTION_NAME).whereEqualTo("profile_id", profileId).limit(1).get();
+        try {
+            List<ProfileId> profileIds = result.get().toObjects(ProfileId.class);
+            return !profileIds.isEmpty();
+        } 
+        catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        } 
+        catch (ExecutionException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return false;
     }
 
     @Override
     public Optional<ProfileId> findByProfileId(String profileId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findByProfileId'");
+        Firestore firestore = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> result =
+                firestore.collection(COLLECTION_NAME).whereEqualTo("profile_id", profileId).limit(1).get();
+        try {
+            List<ProfileId> profileIds = result.get().toObjects(ProfileId.class);
+            if (profileIds.isEmpty()) {
+                return Optional.empty();
+            }
+            return Optional.of(profileIds.get(0));
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (ExecutionException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return Optional.empty();
     }
 
     @Override
     public void deleteByProfileIdAndEntityId(String profileId, String entityId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteByProfileIdAndEntityId'");
+        Firestore firestore = FirestoreClient.getFirestore();
+        Filter profileFilter = Filter.equalTo("profile_id", profileId);
+        Filter entityProfile = Filter.equalTo("entity_id", entityId);
+        Filter filter = Filter.and(profileFilter, entityProfile);
+        ApiFuture<QuerySnapshot> result =
+                firestore.collection(COLLECTION_NAME).where(filter).limit(1).get();
+        try {
+            result.get().forEach(doc -> {
+                doc.getReference().delete();
+            });
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (ExecutionException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
-    
+
 }
