961bbe029fd5948c669b5a89b19809051318df86
vigneshwaran48 <p3487260@gmail.com>
2024-03-16 22:23:49 +0530

    Organization time bug fix

--- application-service/src/main/java/com/vicky/blog/repository/firebase/model/OrganizationModal.java
+++ application-service/src/main/java/com/vicky/blog/repository/firebase/model/OrganizationModal.java
@@ -1,81 +1,83 @@
 package com.vicky.blog.repository.firebase.model;
 
-import java.time.LocalDateTime;
-
 import com.google.cloud.firestore.annotation.PropertyName;
 import com.vicky.blog.common.dto.organization.OrganizationDTO.JoinType;
 import com.vicky.blog.common.dto.organization.OrganizationDTO.Visibility;
 import com.vicky.blog.model.Organization;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 import lombok.Data;
 
 @Data
 public class OrganizationModal {
-    
-    private Long id;
 
-    private String name;
+  private Long id;
 
-    private String owner_id;
+  private String name;
 
-    private String description;
+  private String owner_id;
 
-    private LocalDateTime created_time;
+  private String description;
 
-    private Visibility visibility = Visibility.PUBLIC;
+  private long created_time;
 
-    private JoinType join_type = JoinType.ANYONE;
+  private Visibility visibility = Visibility.PUBLIC;
 
-    private Visibility visibility_string = Visibility.PUBLIC;
-    private JoinType join_type_string = JoinType.ANYONE;
+  private JoinType join_type = JoinType.ANYONE;
 
-    private String image = "http://localhost:7000/static/1";
+  private Visibility visibilityString = Visibility.PUBLIC;
+  private JoinType joinTypeString = JoinType.ANYONE;
 
-    @PropertyName("visibility_string")
-    public String getVisibilityString() {
-        return visibility.name();
-    }
+  private String image = "http://localhost:7000/static/1";
 
-    @PropertyName("visibility_string")
-    public void setVisibilityString(String visiblityString) {
-        visibility_string = Visibility.valueOf(visiblityString);
-        visibility = Visibility.valueOf(visiblityString);
-    }
+  @PropertyName("visibilityString")
+  public String getVisibilityString() {
+    return visibility.name();
+  }
 
-    @PropertyName("join_type_string")
-    public String getJointTypeString() {
-        return visibility.name();
-    }
+  @PropertyName("visibilityString")
+  public void setVisibilityString(String visiblityString) {
+    this.visibilityString = Visibility.valueOf(visiblityString);
+    visibility = Visibility.valueOf(visiblityString);
+  }
 
-    @PropertyName("join_type_string")
-    public void setJointTypeString(String joinTypeString) {
-        join_type_string = JoinType.valueOf(joinTypeString);
-        join_type = JoinType.valueOf(joinTypeString);
-    }
+  @PropertyName("joinTypeString")
+  public String getJoinTypeString() {
+    return join_type.name();
+  }
 
-    public static OrganizationModal build(com.vicky.blog.model.Organization organization) {
-        OrganizationModal organizationModal = new OrganizationModal();
-        organizationModal.setId(organization.getId());
-        organizationModal.setCreated_time(organization.getCreatedTime());
-        organizationModal.setDescription(organization.getDescription());
-        organizationModal.setImage(organization.getImage());
-        organizationModal.setJoin_type(organization.getJoinType());
-        organizationModal.setName(organization.getName());
-        organizationModal.setOwner_id(organization.getOwner().getId());
-        organizationModal.setVisibility(organization.getVisibility());
-        return organizationModal;
-    }
+  @PropertyName("joinTypeString")
+  public void setJoinTypeString(String joinTypeString) {
+    this.joinTypeString = JoinType.valueOf(joinTypeString);
+    join_type = JoinType.valueOf(joinTypeString);
+  }
 
-    public Organization toEntity() {
-        Organization organization = new Organization();
-        organization.setCreatedTime(created_time);
-        organization.setDescription(description);
-        organization.setId(id);
-        organization.setImage(image);
-        organization.setJoinType(join_type);
-        organization.setName(name);
-        organization.setVisibility(visibility);
+  public static OrganizationModal build(
+    com.vicky.blog.model.Organization organization
+  ) {
+    OrganizationModal organizationModal = new OrganizationModal();
+    organizationModal.setId(organization.getId());
+    organizationModal.setCreated_time(Timestamp.valueOf(organization.getCreatedTime()).getTime());
+    organizationModal.setDescription(organization.getDescription());
+    organizationModal.setImage(organization.getImage());
+    organizationModal.setJoin_type(organization.getJoinType());
+    organizationModal.setName(organization.getName());
+    organizationModal.setOwner_id(organization.getOwner().getId());
+    organizationModal.setVisibility(organization.getVisibility());
+    return organizationModal;
+  }
 
-        return organization;
-    }
+  public Organization toEntity() {
+    Organization organization = new Organization();
+    organization.setCreatedTime(Timestamp.from(Instant.ofEpochMilli(created_time)).toLocalDateTime());
+    organization.setDescription(description);
+    organization.setId(id);
+    organization.setImage(image);
+    organization.setJoinType(join_type);
+    organization.setName(name);
+    organization.setVisibility(visibility);
+
+    return organization;
+  }
 }
