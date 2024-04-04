e86d55c40677270979fb303f6db5839b8995e1e9
vigneshwaran48 <p3487260@gmail.com>
2024-04-04 21:53:42 +0530

    Blog public access changes

--- application-service/src/main/java/com/vicky/blog/config/SecurityConfig.java
+++ application-service/src/main/java/com/vicky/blog/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.vicky.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,25 +17,16 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        return httpSecurity
-                    .sessionManagement()
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .csrf().disable()
-                    .authorizeHttpRequests(http -> {
-                        http
-                            .requestMatchers("/api/v1/app/blog/feeds", "/api/v1/app/user/profile")
-                                .permitAll()
-                            .anyRequest()
-                                .authenticated();
-                    })
-                    .oauth2ResourceServer(oauth2 -> oauth2.jwt())
-                    .build();
+        return httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf()
+                .disable().authorizeHttpRequests(http -> {
+                    http.requestMatchers(HttpMethod.GET, "/api/v1/app/blog/feeds", "/api/v1/app/user/profile",
+                            "/api/v1/app/blog/**").permitAll().anyRequest().authenticated();
+                }).oauth2ResourceServer(oauth2 -> oauth2.jwt()).build();
     }
 
     @Bean
-	JwtDecoder jwtDecoder() {
-	    return JwtDecoders.fromIssuerLocation("https://accounts.google.com");
-	}
-    
+    JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("https://accounts.google.com");
+    }
+
 }
