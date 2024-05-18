3de50a31b3dfbea53651d62653d9a2e933eccc3b
vigneshwaran48 <p3487260@gmail.com>
2024-05-18 17:56:48 +0530

    User update changes for allowing to create duplicate emails

--- application-service/src/main/java/com/vicky/blog/repository/mongo/UserMongoRepository.java
+++ application-service/src/main/java/com/vicky/blog/repository/mongo/UserMongoRepository.java
@@ -1,6 +1,6 @@
 package com.vicky.blog.repository.mongo;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -8,5 +8,5 @@ import com.vicky.blog.model.User;
 
 public interface UserMongoRepository extends MongoRepository<User, String> {
     
-    Optional<User> findByEmail(String email);
+    List<User> findByEmail(String email);
 }
