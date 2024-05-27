f3e3c7fd0b7849fdc990de8915717aec23d814a7
vigneshwaran48 <p3487260@gmail.com>
2024-05-27 21:41:47 +0530

    Bots blog changes

--- application-service/src/main/java/com/vicky/blog/listener/DBPopulator.java
+++ application-service/src/main/java/com/vicky/blog/listener/DBPopulator.java
@@ -13,11 +13,17 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.vicky.blog.common.dto.blog.BlogConstants;
+import com.vicky.blog.common.dto.blog.BlogDTO;
 import com.vicky.blog.common.dto.user.UserDTO;
 import com.vicky.blog.common.dto.user.UserDTO.Gender;
 import com.vicky.blog.common.exception.AppException;
+import com.vicky.blog.common.service.BlogService;
+import com.vicky.blog.common.service.ProfileIdService;
 import com.vicky.blog.common.service.TagService;
 import com.vicky.blog.common.service.UserService;
 import com.vicky.blog.model.Tag;
@@ -31,17 +37,24 @@ public class DBPopulator {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ProfileIdService profileIdService;
+
+    @Autowired
+    private BlogService blogService;
+
     @Value("${services.api-gateway.base}")
     private String apiGatewayBase;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DBPopulator.class);
- 
+
     @EventListener(ContextRefreshedEvent.class)
     public void onEvent() {
         try {
             populateTags();
             populateGuestUser();
             createBots();
+            populateBotBlogs();
         } catch (IOException | AppException e) {
             LOGGER.error(e.getMessage(), e);
         }
@@ -93,9 +106,25 @@ public class DBPopulator {
         int max = 6;
         int min = 0;
         int randomNumber = (int) (Math.random() * (max - min)) + min;
-        String offsetStr =  randomNumber == 0 ? "" : "-" + randomNumber;
+        String offsetStr = randomNumber == 0 ? "" : "-" + randomNumber;
         String imageName = "avatar-" + gender.name().toLowerCase() + offsetStr;
         return apiGatewayBase + "/static/" + imageName + ".jpg";
+    }
+
+    private void populateBotBlogs() throws AppException, IOException {
+        UserDTO userDTO = userService.getUser("bot_15").get();
+        ObjectMapper mapper = JsonMapper.builder().addModule(new JavaTimeModule())
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).build();
+        InputStream inputStream = getClass().getResourceAsStream("/blogs.json");
+        List<BlogDTO> blogs = Arrays.asList(mapper.readValue(inputStream, BlogDTO[].class));
+        for (BlogDTO blog : blogs) {
+            LOGGER.info(blog.toString());
+            blog.setOwner(userDTO);
+            blog.setPublised(true);
+            blog.setPublishedAt(profileIdService.getProfileId(userDTO.getProfileId()).get());
+            String blogId = blogService.addBlog(userDTO.getId(), blog);
+            LOGGER.info("Added blog {}", blogId);
+        }
     }
 
 }
--- application-service/src/main/java/com/vicky/blog/service/blog/BlogConstants.java
+++ application-service/src/main/java/com/vicky/blog/service/blog/BlogConstants.java
@@ -2,7 +2,7 @@ package com.vicky.blog.service.blog;
 
 public class BlogConstants {
     
-    public static final int TITLE_MAX_LENGTH = 30;
+    public static final int TITLE_MAX_LENGTH = 70;
     public static final int TITLE_MIN_LENGTH = 3;
     public static final int BLOG_DESCRIPTION_LENGTH = 100;
     public static final String BLOG_EMPTY_TITLE = "Untitled Blog";
